093b57551466448ec91099ee6f2a2c3dd85eab02
github <github>
2021-12-27 23:40:30 +0900

    new app

--- src/index.js
+++ src/index.js
@@ -2,41 +2,66 @@ import React from 'react';
 import reactDOM from 'react-dom';
 import './index.css';
 
-<div id="errors" style="
-  background: #c00;
-  color: #fff;
-  display: none;
-  margin: -20px -20px 20px;
-  padding: 20px;
-  white-space: pre-wrap;
-"></div>
-<div id="root"></div>
-<script>
-window.addEventListener('mousedown', function(e) {
-  document.body.classList.add('mouse-navigation');
-  document.body.classList.remove('kbd-navigation');
-});
-window.addEventListener('keydown', function(e) {
-  if (e.keyCode === 9) {
-    document.body.classList.add('kbd-navigation');
-    document.body.classList.remove('mouse-navigation');
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
   }
-});
-window.addEventListener('click', function(e) {
-  if (e.target.tagName === 'A' && e.target.getAttribute('href') === '#') {
-    e.preventDefault();
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
   }
-});
-window.onerror = function(message, source, line, col, error) {
-  var text = error ? error.stack || error : message + ' (at ' + source + ':' + line + ':' + col + ')';
-  errors.textContent += text + '\n';
-  errors.style.display = '';
-};
-console.error = (function(old) {
-  return function error() {
-    errors.textContent += Array.prototype.slice.call(arguments).join(' ') + '\n';
-    errors.style.display = '';
-    old.apply(this, arguments);
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
   }
-})(console.error);
-</script>
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
